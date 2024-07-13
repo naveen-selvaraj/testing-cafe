b3c153199351a92d851246a50fbbb24e6ee6b071
naveen selvaraj <114022265+naveen-selvaraj@users.noreply.github.com>
2024-07-13 16:46:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,276 @@
+
+  
+  
+    function homepage() {
+        document.getElementById("home").style.display = "block";
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+        
+    }
+
+    function menupage() {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+        burgerpage();
+    }
+
+    function aboutpage() {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("about").style.display = "block";
+        document.getElementById("contact").style.display = "none";
+    }
+
+    function contactpage() {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("contact").style.display = "block";
+    }
+
+    function burgerpage() {
+        document.getElementById("burger").style.display = "block";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+        
+    }
+
+    function momopage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "block";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+
+    function sandwichpage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "block";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+
+    function dessertpage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "block";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+
+    function lolipoppage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "block";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function shakepage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "block";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function friedchickenpage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "block";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function mojitopage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "block";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function starterspage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "block";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function friespage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "block";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function samosapage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "block";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function rollspage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "block";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function icecreampage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "block";
+        document.getElementById("waffle").style.display = "none";
+    }
+    function wafflepage() {
+        document.getElementById("burger").style.display = "none";
+        document.getElementById("momos").style.display = "none";
+        document.getElementById("sandwich").style.display = "none";
+        document.getElementById("desserts").style.display = "none";
+        document.getElementById("lolipop").style.display = "none";
+        document.getElementById("shakes").style.display = "none";
+        document.getElementById("friedchicken").style.display = "none";
+        document.getElementById("mojito").style.display = "none";
+        document.getElementById("starter").style.display = "none";
+        document.getElementById("frenchfries").style.display = "none";
+        document.getElementById("samosa").style.display = "none";
+        document.getElementById("roll").style.display = "none";
+        document.getElementById("ice").style.display = "none";
+        document.getElementById("waffle").style.display = "block";
+    }
+
+
+    homepage();
+
+    const btnLeft = document.querySelector(".left-btn");
+    const btnRight = document.querySelector(".right-btn");
+    const tabMenu = document.querySelector(".tab-menu");
+
+    btnRight.addEventListener("click", () => {
+        tabMenu.scrollLeft += 150;
+    });
+
+    btnLeft.addEventListener("click", () => {
+        tabMenu.scrollLeft -= 150;
+    });
+
